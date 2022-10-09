d280e5ed8809b2653943ca2d19d058d32c0e7aa1
AYOUB-EL <52068209+bbouya@users.noreply.github.com>
2022-10-09 19:45:07 +0100

    Create working_with_vector.cpp

---
+++ working_with_vector.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using std::cout;
+using std::vector;
+
+int main(){
+
+    vector<int> a = {1,2,3,4};
+    // Add some code here to access and print elements of a.
+    std::cout << "a = \n" ;
+
+    for(int i=0; i< a.size(); i++){
+        cout<< a[i]<< " ";
+    }
+    cout << "\n\n";
+
+    vector<vector<int>> b = {
+        {1,1,2,3},
+        {2,1,2,3},
+        {3,1,2,3},
+    };
+    cout << "b = \n";
+    for (int i = 0; i<b.size(); i++){
+        for(int j = 0; j<b[0].size(); j++){
+            
+        }
+    }
+}
